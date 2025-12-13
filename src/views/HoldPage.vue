ebc9ac558ec459438a3b31a5c21fa8e0a3bc7b0c
Fancc666 <fcc565455@163.com>
2025-12-13 14:57:56 +0800

    [update]模版页面和翻页动画

--- src/views/HoldPage.vue
+++ src/views/HoldPage.vue
@@ -1,20 +1,46 @@
 <template>
   <div class="pageContainer">
-    <ContentPage0>0</ContentPage0>
-    <ContentPage0>1</ContentPage0>
-    <ContentPage0>2</ContentPage0>
-    <ContentPage0>3</ContentPage0>
-    <ContentPage0>4</ContentPage0>
-    <ContentPage0>5</ContentPage0>
+    <ContentPage0 :page-id="0" :active-page="activePage">0</ContentPage0>
+    <ContentPage0 :page-id="1" :active-page="activePage">1</ContentPage0>
+    <ContentPage0 :page-id="2" :active-page="activePage">2</ContentPage0>
+    <ContentPage0 :page-id="3" :active-page="activePage">3</ContentPage0>
+    <ContentPage0 :page-id="4" :active-page="activePage">4</ContentPage0>
+    <ContentPage0 :page-id="5" :active-page="activePage">5</ContentPage0>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue';
 import ContentPage0 from './pages/ContentPage0.vue';
+const activePage = ref(-1);
+
+function initPageWatcher() {
+  const scroller = document.querySelector(".pageContainer");
+  const pages = document.querySelectorAll('.page');
+  // console.log(scroller, pages);
+
+  // 1. 观察每一页
+  const io = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      const idx = Array.from(pages).indexOf(entry.target as HTMLElement);
+      if (entry.isIntersecting && idx !== -1) {
+        activePage.value = idx;     // 通知全世界
+      }
+    });
+  }, {
+    root: scroller,      // 关键：滚动的元素
+    threshold: 0.9       // 60% 面积出现就算“进入”
+  });
+
+  // 2. 注册所有页
+  pages.forEach(p => io.observe(p));
+}
+
+onMounted(() => initPageWatcher());
 </script>
 
 <style scoped>
-.pageContainer{
+.pageContainer {
   width: 100%;
   height: 100vh;
   display: flex;
--- src/views/pages/ContentPage0.vue
+++ src/views/pages/ContentPage0.vue
@@ -1,24 +1,64 @@
 <template>
-  <div class="page">
-    <span class="big">
+  <div class="page" ref="page">
+    <span class="big text box">
       <slot></slot>
     </span>
+    <span class="text box">hello world <slot></slot></span>
+    <span class="text box">hello world <slot></slot></span>
+    <span class="text box">hello world <slot></slot></span>
+    <span class="text box">hello world <slot></slot></span>
+    <span class="text box">hello world <slot></slot></span>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, watch } from 'vue';
+const page = ref<HTMLElement>();
+const timers: number[] = [];
+function clearAnimate() {
+  timers.forEach(t => clearTimeout(t));
+  page.value?.querySelectorAll(".box").forEach(e => {
+    e.classList.remove("an-slidein");
+  });
+}
+function applyAnimate() {
+  let count = 0;
+  page.value?.querySelectorAll(".box").forEach(e => {
+    timers.push(
+      setTimeout(() => {
+        e.classList.add("an-slidein");
+      }, 450 * count)
+    );
+    count++;
+  });
+}
 
+const props = defineProps<{ activePage: number, pageId: number }>();
+watch(() => props.activePage, () => {
+  if (props.activePage !== props.pageId) {
+    clearAnimate();
+    return;
+  }
+  console.log("play animation:", props.activePage);
+  applyAnimate();
+});
 </script>
 
 <style scoped>
 .page {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);;
+  background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+  ;
 }
-.big{
+
+.big {
   font-size: 2em;
+}
+
+.text {
   color: white;
 }
 </style>
